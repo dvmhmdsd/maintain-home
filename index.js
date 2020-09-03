16b016f7633cab2019d5497d12ce99722c8349c0
Mohamed <mahameds19999@gmail.com>
2020-09-03 22:11:32 +0200

    Setup the project v1

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "public")));
+
+  // always send the index.html file to handle SPA routing
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+}
+
+// disable the X-Powered-By header instead of using helmet
+app.disable("x-powered-by");
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Listening to port ${port}`));
