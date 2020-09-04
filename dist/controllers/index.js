6a5dd62e4e776a07180471ea0cdc78bc3e083b82
Mohamed <mahameds19999@gmail.com>
2020-09-04 16:51:47 +0200

    Create dashboard

--- dist/controllers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userController = void 0;
-const user_controller_1 = __importDefault(require("./user/user.controller"));
-exports.userController = user_controller_1.default;
-//# sourceMappingURL=index.js.map
