7125b2a096af4ef48da4699802e7d733d9381323
Mohamed <mahameds19999@gmail.com>
2020-09-12 22:21:06 +0200

    [Front] Add lookups

--- dashboard/src/app/home/home.component.ts
+++ dashboard/src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { LookupsService } from './../services/lookups.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  ordersCount: number;
+  complaintsCount: number;
+  constructor(private lookups: LookupsService) {}
 
   ngOnInit(): void {
+    this.lookups.get().subscribe(
+      (res: any) => {
+        this.ordersCount = res.orders;
+        this.complaintsCount = res.complaints;
+      },
+      () => {
+        alert('حدث خطأ أثناء جلب البيانات، حاول مرة أخري');
+      }
+    );
   }
-
 }
