05d52fa1bc0e7a7880e3acfa2d3ea8cf8ecbc447
Mohamed <mahameds19999@gmail.com>
2020-10-23 18:32:30 +0200

    Remove config folder from ignoring

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,20 @@
+import multer from "multer";
+// @ts-ignore
+import cloudinaryStorage from "multer-storage-cloudinary";
+
+const cloudinary = require("cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  folder: "demo",
+});
+
+const parser = multer({ storage });
+
+export { parser };
---
+++ src/config/mailer.ts
@@ -0,0 +1,14 @@
+import sgMail from "@sendgrid/mail";
+import dotenv from "dotenv";
+
+dotenv.config();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const templates = {
+  client_order_updated: "d-92d6449bd4f740e8bc9560b65dc90e61",
+  client_order_created: "d-5ff67f1523674ad492da0f7c044e627c",
+  admin_order: "d-1ca8d83609a848e7bc819ac8c33e8ea0",
+  complaint: "d-34083fa9067a4598839a8ec0938d413d",
+};
+
+export { sgMail, templates };
